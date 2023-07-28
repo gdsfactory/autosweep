34b24507d53437874f6d24063fe1ea1f4089ea74
vgvelev <136128676+vgvelev@users.noreply.github.com>
2023-07-28 14:39:26 -0700

    more doc strings

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+install_requires = ['numpy', 'matplotlib', 'orjson']
+
 setup(name='ta',
       version='0.0.1',
-      install_requires=['orjson'])
+      install_requires=install_requires)
