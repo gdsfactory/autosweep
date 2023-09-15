601f540d4955c0dd5fc580236a2262b7ee798138
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-09-14 21:42:21 -0700

    fix tests

--- tests/base/testing_io.py
+++ tests/base/testing_io.py
@@ -3,13 +3,15 @@ import logging
 from autosweep.utils.io import read_json, write_json
 from autosweep.utils.logger import init_logger
 
-# init_logger(path='testing.txt')
-init_logger()
-data = {"hello": {"1": "a", "b": 2}, "world": "123"}
-filename = "temp.json"
 
-write_json(data=data, path=filename)
-data_in = read_json(path=filename)
+def test_io() -> None:
+    # init_logger(path='testing.txt')
+    init_logger()
+    data = {"hello": {"1": "a", "b": 2}, "world": "123"}
+    filename = "temp.json"
 
-assert data == data_in, "The data written should be the data read back"
-logging.info("Passed")
+    write_json(data=data, path=filename)
+    data_in = read_json(path=filename)
+
+    assert data == data_in, "The data written should be the data read back"
+    logging.info("Passed")
--- tests/instruments/instr_test.py
+++ tests/instruments/instr_test.py
@@ -1,19 +1,22 @@
-from pathlib import Path
+import pathlib
 
 import new_stuff
 
 import autosweep
 from autosweep.utils import registrar
 
-registrar.register_classes(new_stuff)
-autosweep.init_logger()
 
-station_cfg = autosweep.StationConfig.read_json(
-    path=Path("../test_exec/station_config.json")
-)
+def test_instr() -> None:
+    registrar.register_classes(new_stuff)
+    autosweep.init_logger()
 
-with autosweep.InstrumentManager(station_config=station_cfg) as i:
-    i.load_instruments(instr_names="all")
+    dirpath = pathlib.Path(__file__).parent.absolute()
+    station_cfg = autosweep.StationConfig.read_json(
+        path=dirpath / "test_exec" / "station_config.json"
+    )
 
-print(station_cfg.instruments)
-print(station_cfg.station_config)
+    with autosweep.InstrumentManager(station_config=station_cfg) as i:
+        i.load_instruments(instr_names="all")
+
+    print(station_cfg.instruments)
+    print(station_cfg.station_config)
